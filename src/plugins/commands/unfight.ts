313459f0b16cb4e3e827b9805063eb3897e6b51c
interrrp <int@addictedto.coffee>
2024-04-08 13:50:00 +0800

    Simplify `unfight`

--- src/plugins/commands/unfight.ts
+++ src/plugins/commands/unfight.ts
@@ -1,12 +1,10 @@
 import { Command } from "@/plugins/commands";
-import { findPlayer } from "@/utils/minecraft";
 
 export default {
   name: "unfight",
   aliases: ["stop-fighting", "unpvp"],
   description: "Stops fighting.",
-  execute(bot, args, sender) {
-    const player = findPlayer(bot, sender, args[0] ?? "");
-    if (player) bot.pvp.stopFighting();
+  execute(bot) {
+    bot.pvp.stopFighting();
   },
 } as Command;
