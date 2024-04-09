0bf4466153cf1c91cff18ea0417531ab9049b57c
interrrp <int@addictedto.coffee>
2024-04-09 14:37:03 +0800

    Use TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["./web/index.html", "./web/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- web/main.tsx
+++ web/main.tsx
@@ -14,6 +14,7 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
+import "./styles.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
